0253581649968a546f383bc13cfbb06b268d7d52
adehenry <adefemiadelaja@gmail.com>
2022-12-06 21:48:16 +0100

    get top three fuction

---
+++ miscelleneous/topThree.js
@@ -0,0 +1,32 @@
+const arr = [2, 58, 53, 6 ,84 ,24 ,6 ,72 ,10 ,78,21 ,1 ,3] 
+
+function getTopThree(arr) {
+
+    let first = 0
+    let second = 0
+    let third = 0
+
+for (let num of arr){
+
+  if (num > first) {
+    third = second
+    second = first
+    first = num
+  }
+  if (num > second && num < first){
+    third = second
+    second = num
+  }
+  if (num > third && num < second){
+    third = num
+  }
+}
+
+ return {first, second, third}
+}
+
+
+
+const {first, second, third} = getTopThree(arr)
+
+console.log({first, second, third})
