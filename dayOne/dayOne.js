d2fba0b5af3c60a82fc4e87872a733a9ce69158e
adehenry <adefemiadelaja@gmail.com>
2022-12-06 15:42:16 +0100

    day one challenge

--- dayOne/dayOne.js
+++ dayOne/dayOne.js
@@ -1,29 +1,36 @@
 const fs = require("fs")
 
 function readFileContent(filePath) {
-    let result
+
     fs.readFile(filePath, "utf-8", (error, data) => {
         if (error) {
             console.log(error.message)
             return error.message
         }
        const array = data.split("\n")
-      const {highest, highestIndex} = sumArray(array)
+      const {resultArray,highest, highestIndex} = sumArray(array)
+
+       const topThree = resultArray.sort((a, b) => b - a).slice(0, 3)
+       let topThreeSum = 0
+       for (let num of topThree) topThreeSum += num
+       
        console.log({highest})
        console.log({highestIndex})
+       console.log({topThreeSum})
+
     })
 }
 
    function sumArray(array) {
     let highest = 0
     let highestIndex = 0
-    let result = []
+    let resultArray = []
     let accumulator = 0
 
     for (let i = 0; i < array.length - 1; i++){
       if (array[i] === "\r"){
-        result.push(accumulator)
-        if(accumulator > highest) highestIndex = result.length - 1
+        resultArray.push(accumulator)
+        if(accumulator > highest) highestIndex = resultArray.length - 1
         highest = Math.max(highest, accumulator)
         accumulator = 0
         continue
@@ -32,7 +39,8 @@ function readFileContent(filePath) {
       let newItem = array[i].slice(0, -1)
       accumulator += Number(newItem)
     }
-    return {highest, highestIndex}
+    return {resultArray,highest, highestIndex}
 }
 
  readFileContent("input.txt")
+
